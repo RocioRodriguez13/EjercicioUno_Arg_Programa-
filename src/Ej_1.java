0ba393d759aeb670df61e3f0fd0f505fe952aa3d
rocio <rorodriguez.222@gmail.com>
2023-03-22 09:04:27 -0300

    primer commit

---
+++ src/Ej_1.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rocio r
+ */
+
+public class Ej_1 {
+   
+   public static void main(String[] args) {
+        
+//        int contador = 5;
+//        int numeroFin = 14;
+////
+////   //A.
+//   while (contador <= numeroFin) {
+//            System.out.println(contador);
+//            contador++;
+////B.
+////           if (contador % 2 == 0) {
+////                System.out.println("Es par:" + contador);
+////
+////            }
+////C.
+//            if (contador % 2 == 0) { 
+//                System.out.println("Es par: "+ contador);
+//            } else { System.out.println("Es impar: " + contador);
+//            }}
+////
+//////D.
+////
+////        System.out.println("Los numeros");
+//////
+////    for (numeroFin = 14; numeroFin >= contador; numeroFin --) {
+////            if (numeroFin % 2 == 0)
+////                System.out.println("Los numeros pares son: " + numeroFin);
+////     
+////     }
+     
+     
+   //  Ejercicio dos //
+
+           
+float ingresos = 339000;
+int superficie = 80;
+int energia = 900;
+/*********************************************/
+if ( (ingresos <= 748382.07) &&
+( superficie <= 30) &&
+(energia <= 3330) ) {
+System.out.println("Categoría A");
+} else if((ingresos <= 1112459.83) &&
+(superficie <= 45) &&
+(energia <= 5000) ) {
+System.out.println("Categoría B");
+}else if  ((ingresos <= 1557443.75) && (superficie <= 60) && (energia <= 6700)) {
+ System.out.println("Categoría C");
+} else if  ((ingresos <= 1557443.75) && (superficie <= 60) && (energia <= 6700)) {
+                System.out.println("Categoría C");
+                } else if((ingresos <= 1943273.04) && (superficie <= 85) &&
+                (energia <= 100000)) {
+                System.out.println("Categoría D");
+                }else if((ingresos <= 2277684) && (superficie <= 110) &&
+                (energia <= 130000) ) {
+                System.out.println("Categoría E");
+    }
+   
+   
+   
+   
+}
+
+}
+
+
---
+++ src/ej.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rocio r
+ */
+public class ej {
+    
+    //public static void main(String[] args) {
+        
+        System.out.println("hola");
+        
+float ingresos = 339000;
+int superficie = 80;
+int energia = 900;
+/*********************************************/
+if ( (ingresos <= 748382.07) &&
+( superficie <= 30) &&
+(energia <= 3330) ) {
+System.out.println("Categoría A");
+} else if((ingresos <= 1112459.83) &&
+(superficie <= 45) &&
+(energia <= 5000) ) {
+System.out.println("Categoría B");
+}else if  ((ingresos <= 1557443.75) && (superficie <= 60) && (energia <= 6700)) {
+ System.out.println("Categoría C");
+} else if  ((ingresos <= 1557443.75) && (superficie <= 60) && (energia <= 6700)) {
+                System.out.println("Categoría C");
+                } else if((ingresos <= 1943273.04) && (superficie <= 85) &&
+                (energia <= 100000)) {
+                System.out.println("Categoría D");
+                }else if((ingresos <= 2277684) && (superficie <= 110) &&
+                (energia <= 130000) ) {
+                System.out.println("Categoría E");
+    }
+    
+}
+}
